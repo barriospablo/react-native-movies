7799f28d791f569da87352ceef3b4ce9e1368a67
Pablo Barrios <exepablo96@gmail.com>
2025-10-20 18:18:17 -0300

    map response data

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,6 +3,8 @@ import { Text, View } from "react-native";
 import "../global.css";
 
 const RootLayout = () => {
+  //aca probamos que la funcion llame bien a la api y nos devuelva la data mapeada
+  //nowPlayingAction();
   return (
     <View>
       <Text>RootLayout</Text>
---
+++ core/actions/movies/now-playing.ts
@@ -0,0 +1,16 @@
+import { movieApi } from "@/core/api/movie-api";
+import { MovieDBMoviesResponse } from "@/infrastructure/interfaces/movieDb-response";
+import { MovieMapper } from "@/infrastructure/mappers/movie.mapper";
+
+export const nowPlayingAction = async () => {
+  try {
+    const { data } = await movieApi.get<MovieDBMoviesResponse>("/now_playing");
+    const movies = data.results.map(MovieMapper.fromTheMovieDBToMovie);
+    console.log(movies);
+
+    return movies;
+  } catch (error) {
+    console.log(error);
+    throw "Cannot load now playing movies";
+  }
+};
